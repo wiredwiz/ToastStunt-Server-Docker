193eeb9845c8eab3ffe42e61aa928da4ef97eb4c
Thaddeus Ryker <thad@edgerunner.org>
2022-07-10 14:37:30 -0400

    Added scripts and a build file for creating a test tag.  This is used to test new features.

---
+++ startup.sh
@@ -0,0 +1,8 @@
+if [ ! -z "$PUID" ] && [ ! -z "$PGID" ]; then
+    groupmod -o -g "$PGID" moo
+    usermod -o -u "$PUID" moo
+    chown -R moo:moo /home/*
+	gosu moo restart "$1"
+else
+	restart "$1"
+fi
